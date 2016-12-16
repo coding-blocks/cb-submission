4111e7afbc4f0c3d9b3e21453a66b30e14cdb21d
Umair Khan <omerjerk@gmail.com>
2016-12-16 11:48:05 +0530

    add judge function to exports
    Signed-off-by: Umair Khan <omerjerk@gmail.com>

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 /**
  * Created by umair on 16/12/16.
  */
+
+var request = require("request");
+
+const URL = "http://cb.lk:3000/api/";
+
+module.exports = {
+    judge: function(lang, source, testcases, callback) {
+        request({
+            uri: URL + "submission",
+            method: "POST",
+            form: {
+                lang: lang,
+                source: source,
+                testcases: testcases
+            }
+        }, function(error, response, body) {
+            console.log(body);
+            callback(body);
+        });
+    }
+}
