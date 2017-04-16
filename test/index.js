d9a342a19fdf7acaccf83ca7dd233d5138faa57a
Prajjwal Singh <sin@prajjwal.com>
2017-04-16 17:12:48 +0530

    Fix: Everything

--- test/index.js
+++ test/index.js
@@ -1,72 +1,45 @@
-/**
- * Created by umair on 16/12/16.
- */
-
-var should = require('chai').should(),
+let should = require('chai').should(),
     expect = require('chai').expect,
     base64 = require("base-64"),
     submission = require('../index'),
-    judge = submission.judge;
+    R = require('ramda'),
+    util = require('../util/util'),
+    { judge } = submission
 
-describe('#Test C submissions', function () {
+describe('#Test C submissions', () => {
+  it('returns success for a trivial, and correct sample', (done) => {
+    let source = '#include<stdio.h> \nint main(void) {\nint a, b, c; \nscanf("%d%d", &a, &b); \nc = a + b; printf("%d", c);\n}'
+    let testcases = ["1 2"]
+    let expected = ["3"]
+    let test_count = 1
 
-    it('judges a trivial c submission to add two numbers', function (done) {
-        var source = '#include<stdio.h> \nint main() {\nint a, b, c; \nscanf("%d%d", &a, &b); \nc = a + b; printf("%d", c);\n}';
-        var testcases = ["1 2"];
-        var expected = ["3"];
-        var test_count = 1;
-        judge("c", source, test_count, testcases, expected, true, function (body) {
-            body = JSON.parse(body);
-            body.result.should.equal("success");
-            var testcases = body.data.testcases;
-            for (var i = 0; i < testcases.length; ++i) {
-                testcases[i].result.should.equal("correct");
-                var output = base64.decode(testcases[i].output);
-                output.should.equal("3");
-            }
-            done();
-        });
-    }).timeout(5000);
+    judge("c", source, test_count, testcases, expected, true, (body) => {
+      util.assertResult(body, "success", ["correct"], expected)
+      done()
+    })
+  }).timeout(5000)
 
-    it('judges a submission and make sure output is not present', function (done) {
-        var source = '#include<stdio.h> \nint main() {\nprintf("Hello World!");\n}';
-        var testcases = [""];
-        var expected = ["Hello World!"];
-        var test_count = 1;
-        judge("c", source, test_count, testcases, expected, false, function (body) {
-            body = JSON.parse(body);
-            body.result.should.equal("success");
-            var testcases = body.data.testcases;
-            for (var i = 0; i < testcases.length; ++i) {
-                testcases[i].result.should.equal("correct");
-                expect(testcases[i].output).to.be.undefined;
-            }
-            done();
-        });
-    }).timeout(5000);
+  it('returns wrong-answer for unexpected output', (done) => {
+    let source = '#include<stdio.h> \nint main(void) {\nint a, b, c; \nscanf("%d%d", &a, &b); \nc = a + b; printf("%d", c);\n}'
+    let testcases = ["2 2"]
+    let expected = ["3"]
+    let test_count = 1
 
-    it('check if an incorrect source gives out a compilation error', function (done) {
-        var source = '#include<stdio.h> \nint main() {\nprintf("Hello World!")\n}';
-        var testcases = [""];
-        var expected = [""];
-        var test_count = 1;
-        judge("c", source, test_count, testcases, expected, true, function (body) {
-            body = JSON.parse(body);
-            body.result.should.equal("compile_error");
-            done();
-        })
-    }).timeout(5000);
+    judge("c", source, test_count, testcases, expected, true, (body) => {
+      util.assertResult(body, "success", ["wrong-answer"], ['4'])
+      done()
+    })
+  }).timeout(5000)
 
-    it('checks if the wrong output gives wrong-answer as the response', function (done) {
-        var source = '#include<stdio.h> \nint main() {\nprintf("Hello World!");\n}';
-        var testcases = [""];
-        var expected = [""];
-        var test_count = 1;
-        judge("c", source, test_count, testcases, expected, true, function (body) {
-            body = JSON.parse(body);
-            body.result.should.equal("success");
-            body.data.testcases[0].result.should.equal("wrong-answer");
-            done();
-        });
-    });
-});
+  it('returns a compile error when the sample is not valid C', (done) => {
+    let source = '#inklewd<studi-oh.h> \nlint pain(droid) {\nint a, b, c; \nscanf("%d%d", &a, &b); \nc = a + b; printf("%d", c);\n}'
+    let testcases = [""]
+    let expected = [""]
+    let test_count = 1
+
+    judge("c", source, test_count, testcases, expected, true, ({ result, error }) => {
+      result.should.equal("compile_error")
+      done()
+    })
+  }).timeout(5000)
+})
--- test/python.js
+++ test/python.js
@@ -1,17 +1,51 @@
-var should = require('chai').should(),
+let should = require('chai').should(),
     expect = require('chai').expect,
     base64 = require("base-64"),
+    R = require('ramda'),
     submission = require('../index'),
-    judge = submission.judge;
+    util = require('../util/util.js'),
+    { judge } = submission
 
-describe('#Test Python submissions', function () {
-    it('judges a trivial python submission', function (done) {
-        var source = 'print("Hello World!")';
-        var testcases = [""];
-        var expected = ["Hello World!"];
-        var test_count = 1;
-        judge("py2", source, test_count, testcases, expected, true, function (body) {
-            done();
-        });
-    });
-});
+const TIMEOUT = 5000
+
+describe('#Test Python submissions', () => {
+  it('returns success for a trivial, and correct python sample', (done) => {
+    let source = 'print("Hello World!")',
+        testcases = [""],
+        expected = ["Hello World!\n"],
+        test_count = 1
+
+    judge("py2", source, test_count, testcases, expected, true, (body) => {
+      util.assertResult(body, "success", ["correct"], expected)
+      done()
+    })
+  }).timeout(TIMEOUT)
+
+  it('indicates wrong-answer for a python sample that returns incorrect output', (done) => {
+    let source = 'print("Obviously Incorrect Output")',
+        testcases = [""],
+        expected = ["Hello World!\n"],
+        test_count = 1
+
+    judge("py2", source, test_count, testcases, expected, true, (body) => {
+      util.assertResult(body, "success", ["wrong-answer"], ["Obviously Incorrect Output\n"])
+      done()
+    })
+  }).timeout(TIMEOUT)
+
+  it('returns compilation_error when python sample is invalid', (done) => {
+    let source = 'int main(void) { return 0; }',
+        testcases = [""],
+        expected = [""],
+        test_count = 1
+
+    judge("py2", source, test_count, testcases, expected, true, ({ result, error }) => {
+      result.should.equal("compile_error")
+      done()
+    })
+  }).timeout(TIMEOUT)
+
+  // TODO
+  // -- Test nontrivial python samples
+  // -- Test samples with multiple inputs
+})
