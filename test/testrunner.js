e9229d7f3874cbbe3e94e85e7be665c55f7322d0
Siddharth Dungarwal <sd5869@gmail.com>
2017-07-17 16:44:28 +0530

    Added e-mail support when judge is down.

--- test/testrunner.js
+++ test/testrunner.js
@@ -4,7 +4,6 @@ let mail = require('../mail/mail.js');
 
 let runTests = () => {
   let isJudgeDown = false;
-
   mocha.addFile('test/index.js');
   mocha.addFile('test/python.js');
   mocha.addFile('test/java.js');
@@ -18,11 +17,12 @@ let runTests = () => {
     .on('fail', (test, error) => {
       console.error('Test fail');
       isJudgeDown = true;
+    })
+    .on('end',(test) => {
+      if(isJudgeDown){
+        mail.sendMail();
+      }
     });
-
-  if (isJudgeDown) {
-    mail.sendMail();
-  }
 }
 
 module.exports.runTests = runTests;
